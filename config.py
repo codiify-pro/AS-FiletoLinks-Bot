d1d6d2f89893291dd137302ce7d0af24876939c5
Codiify <aaryansah954@gmail.com>
2026-05-20 00:46:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ id_pattern = re.compile(r'^-?\d+$')
 
 
 SESSION = environ.get("SESSION", "UHDFiletoLinksBot")
-API_ID = int(environ.get("API_ID", "0"))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "32541562"))
+API_HASH = environ.get("API_HASH", "e37e4432298d5a5eb4a6e32c18804283")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8932447404:AAHyanPJNFCm1yK7pA6hch2lnvMichi3qMI")
 
 
 PORT = int(environ.get("PORT", "8080"))
@@ -25,12 +25,12 @@ ON_HEROKU = "DYNO" in environ
 URL = environ.get("URL", "")
 
 
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "0"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002521835919"))
 ADMINS = [
     int(admin) if id_pattern.match(admin) else admin
-    for admin in environ.get("ADMINS", "").split()
+    for admin in environ.get("ADMINS", "2021145517").split()
 ]
 
 
 DATABASE_URI = environ.get("DATABASE_URI", "")
-DATABASE_NAME = environ.get("DATABASE_NAME", "")
+DATABASE_NAME = environ.get("DATABASE_NAME", "file")
